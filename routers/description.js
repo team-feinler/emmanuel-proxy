fda0405de3607e946eee2af2aefdfd563f761d55
samasno <samasno@LAPTOP-HJT8OQVR.localdomain>
2021-04-09 17:17:57 +0400

    added cache getter middleware to GET@/description/:productId

--- routers/description.js
+++ routers/description.js
@@ -1,11 +1,12 @@
 const { forwardingController } = require('../controllers/forwarding.js');
 const { asyncHandler } = require('../controllers/utils.js');
 const router = require('express').Router();
+const { getCached } = require('../controllers/redis.js');
 
 
 // router for requests to description service
 router.route('/description/:productId')
-  .get(asyncHandler(forwardingController('DESCRIPTION', 4004)))
+  .get(getCached, asyncHandler(forwardingController('DESCRIPTION', 4004)))
   .put(asyncHandler(forwardingController('DESCRIPTION', 4004)))
   .delete(asyncHandler(forwardingController('DESCRIPTION', 4004)));
 router.route('/descriptions/new')
